ccf865cb93c7791439af77629e856d9e0c19e8d4
mkozlowska <m.d.kozlowska@gmail.com>
2016-04-26 18:06:00 +0200

    Project initialization resolves #1

---
+++ src/main/java/com/taskOrganizer/TaskOrganizerApplication.java
@@ -0,0 +1,12 @@
+package com.taskOrganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskOrganizerApplication.class, args);
+	}
+}
