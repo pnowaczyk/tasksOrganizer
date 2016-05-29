597f73e2a4642f83edf3e9a1e54b2f784cb7d2a6
mkozlowska <m.d.kozlowska@gmail.com>
2016-05-29 11:36:44 +0200

    adding external properties file for mongoDB

--- src/main/java/com/taskOrganizer/conf/MongoConfig.java
+++ src/main/java/com/taskOrganizer/conf/MongoConfig.java
@@ -2,6 +2,7 @@ package com.taskOrganizer.conf;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -12,17 +13,37 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 
 @Configuration
 @EnableMongoRepositories(basePackages = "com.taskOrganizer.model")
+@ConfigurationProperties(locations = "classpath:mongoDB.properties", prefix = "mongo")
 public class MongoConfig extends AbstractMongoConfiguration {
+    private String databaseName;
+    private String dbPort;
+    private String mappingBasePackage;
 
+    public void setDatabaseName(String databaseName) {
+        this.databaseName = databaseName;
+    }
 
     @Override
-    protected String getDatabaseName() {
-        return "tasksDemoDb";
+    public String getDatabaseName() {
+        return databaseName;
+
+    }
+
+    public String getDbPort() {
+        return dbPort;
+    }
+
+    public void setDbPort(String dbPort) {
+        this.dbPort = dbPort;
+    }
+
+    public void setMappingBasePackage(String mappingBasePackage) {
+        this.mappingBasePackage = mappingBasePackage;
     }
 
     @Override
     public Mongo mongo() throws Exception {
-        return new MongoClient("127.0.0.1", 27017);
+        return new MongoClient(dbPort, 27017);
     }
 
     @Override
